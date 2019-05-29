1b9e9035c7f6c8c53e0a4da9b9a275348b69eac8
Ahmed El-Araby <araby2305@gmail.com>
2019-05-29 02:17:54 +0200

    changed categories owl carousel to init after page load

--- app/design/frontend/Vigor/hedeya/web/js/custom.js
+++ app/design/frontend/Vigor/hedeya/web/js/custom.js
@@ -5,19 +5,21 @@ require([
 ], function ($) {
     'use strict';
 
-    jQuery(".categories-owl").owlCarousel({
-        autoPlay : true,
-        items : 4,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [980,3],
-        itemsTablet: [768,2],
-        itemsMobile : [479,1],
-        slideSpeed : 500,
-        paginationSpeed : 500,
-        rewindSpeed : 500,
-        navigation : false,
-        stopOnHover : true,
-        pagination :false,
-        scrollPerPage:true,
+    jQuery(document).ready(function() {
+        jQuery(".categories-owl").owlCarousel({
+            autoPlay : true,
+            items : 4,
+            itemsDesktop : [1199,4],
+            itemsDesktopSmall : [980,3],
+            itemsTablet: [768,2],
+            itemsMobile : [479,1],
+            slideSpeed : 500,
+            paginationSpeed : 500,
+            rewindSpeed : 500,
+            navigation : false,
+            stopOnHover : true,
+            pagination :false,
+            scrollPerPage:true,
+        });
     });
 });
