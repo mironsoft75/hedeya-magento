200227a7461487c6506a1cb52d0fb43e76a164c9
Ahmed Elaraby <araby2305@gmail.com>
2019-09-26 05:21:19 +0200

    Fixed Hedeya Mobile API deprecated return type issue

--- app/code/Hedeya/MobileApi/Api/Data/Catalog/SorterInterface.php
+++ app/code/Hedeya/MobileApi/Api/Data/Catalog/SorterInterface.php
@@ -7,17 +7,17 @@ namespace Hedeya\MobileApi\Api\Data\Catalog;
 interface SorterInterface
 {
     /**
-     * @return \Magento\Framework\Option\ArrayInterface[]
+     * @return \Magento\Framework\Data\OptionSourceInterface[]
      */
     public function getDirection();
 
     /**
-     * @return \Magento\Framework\Option\ArrayInterface[]
+     * @return \Magento\Framework\Data\OptionSourceInterface[]
      */
     public function getPageSize();
 
     /**
-     * @return \Magento\Framework\Option\ArrayInterface[]
+     * @return \Magento\Framework\Data\OptionSourceInterface[]
      */
     public function getSortBy();
 }
